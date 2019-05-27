9d7fa5aa7248fe39f2f8b45e8612f801592cd8b6
communal-principle-3181 <aellis19@outlook.com>
2019-05-27 22:54:04 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,12 +3,19 @@ function getIt()
 {
   $('p').on('click', function(){alert('Hey!')})
 }
+
 function frameIt()
 {
   $('img').on('load',function(){
   $('img').addClass('tasty');
   })
 }
+
+function pressIt()
+{
+  $('')
+}
+
 $(document).ready(function(){
 
 // call functions here
